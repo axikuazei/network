610ed583faaab8ebcd4c47c29c752bf7ba01f771
axikuazei <axikuazei@outlook.com>
2022-09-21 09:03:30 +0800

    update

--- hello_server.c
+++ hello_server.c
@@ -1,6 +1,24 @@
 # include <stdio.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    printf("hello world\n");
+
+    int serv_sock;
+    int clnt_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[] = "Hello World";
+
+    if (argc != 2)
+    {
+        printf("Usage : %s <port> \n", argv[0]);
+    }
+    
+    
 }
